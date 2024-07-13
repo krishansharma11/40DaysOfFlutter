325d11911cbe33fa91e17d476cc8ebbcf4794df5
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-13 18:44:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ButtonClass());
+}
+
+class ButtonClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Buttons Examples"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("Elevated Button"),
+              ElevatedButton.icon(
+                onPressed: () {
+                  print("Elevated Button Pressed");
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue, // Text color
+                ),
+                label: Text("Elevated Button"),
+                icon: Icon(Icons.add),
+              ),
+              SizedBox(height: 16), // Spacing between buttons
+              Text("Text Button"),
+              TextButton(
+                onPressed: () {
+                  print("Text Button Pressed");
+                },
+                child: Text("Text Button"),
+              ),
+              SizedBox(height: 16),
+              Text("Outlined Button"),
+              OutlinedButton(
+                onPressed: () {
+                  print("Outlined Button Pressed");
+                },
+                child: Text("Outlined Button"),
+              ),
+              SizedBox(height: 16),
+              Text("Icon Button"),
+              IconButton(
+                icon: Icon(Icons.camera_alt_outlined),
+                onPressed: () {
+                  print("Icon Button Pressed");
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
