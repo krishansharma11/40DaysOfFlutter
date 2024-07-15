2174fa7855bb9b0bdb1a0576ac0a603f8f910347
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-15 13:02:48 +0530

    basic of flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,70 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(ButtonClass());
+void greet(String name) {
+  print('Hello, $name');
 }
 
-class ButtonClass extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Buttons Examples"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text("Elevated Button"),
-              ElevatedButton.icon(
-                onPressed: () {
-                  print("Elevated Button Pressed");
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.blue, // Text color
-                ),
-                label: Text("Elevated Button"),
-                icon: Icon(Icons.add),
-              ),
-              SizedBox(height: 16), // Spacing between buttons
-              Text("Text Button"),
-              TextButton(
-                onPressed: () {
-                  print("Text Button Pressed");
-                },
-                child: Text("Text Button"),
-              ),
-              SizedBox(height: 16),
-              Text("Outlined Button"),
-              OutlinedButton(
-                onPressed: () {
-                  print("Outlined Button Pressed");
-                },
-                child: Text("Outlined Button"),
-              ),
-              SizedBox(height: 16),
-              Text("Icon Button"),
-              IconButton(
-                icon: Icon(Icons.play_arrow),
-                onPressed: () {
-                  print("Icon Button Pressed");
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+int add(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  //Varialbles
+  var name = 'John'; //changeble
+  final age = 35; //when its used then initialied
+  const pi = 3.14; // constant at compile time
+
+  print(name);
+  print(age);
+  print(pi);
+
+//data type
+  int agePerson = 30;
+  double height = 5.9;
+  String namePerson = 'John';
+  bool isStudent = true;
+  List<String> colors = ['Red', 'Green', 'Blue'];
+
+  print(age);
+  print(height);
+  print(name);
+  print(isStudent);
+  print(colors);
+
+//functions
+  greet("Willium");
+  add(10, 20);
+
+  //Control Flow
+  int number = 10;
+
+  // if-else statement
+  if (number > 0) {
+    print('Positive');
+  } else {
+    print('Negative');
+  }
+
+  // for loop
+  for (int i = 0; i < 5; i++) {
+    print(i); // Output: 0 1 2 3 4
+  }
+
+  // while loop
+  int count = 0;
+  while (count < 5) {
+    print(count); // Output: 0 1 2 3 4
+    count++;
+  }
+
+  // switch statement
+  String grade = 'A';
+  switch (grade) {
+    case 'A':
+      print('Excellent');
+      break;
+    case 'B':
+      print('Good');
+      break;
+    default:
+      print('Invalid grade');
   }
 }
