9151e4c70122a5a3fbd89c31f25bca164ea3d0f3
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-15 15:50:47 +0530

    statefullwidget

--- day2_widgets/lib/main.dart
+++ day2_widgets/lib/main.dart
@@ -5,23 +5,69 @@ void main() {
   runApp(MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Stateless Widget Example')),
+//         body: Center(
+//           child: MyStatelessWidget(),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class MyStatelessWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text('Hello, I am a Stateless Widget');
+//   }
+// }
+
+// 2. Stateful Widgets
+// A StatefulWidget is a widget that does require mutable state, meaning that it can change over time. This is useful when the UI can change dynamically, such as with user interactions or data updates. Stateful widgets consist of two classes: the StatefulWidget itself and the State class.
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Stateless Widget Example')),
+        appBar: AppBar(title: Text('Stateful Widget Example')),
         body: Center(
-          child: MyStatelessWidget(),
+          child: MyStatefulWidget(),
         ),
       ),
     );
   }
 }
 
-class MyStatelessWidget extends StatelessWidget {
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Hello, I am a Stateless Widget');
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('Button pressed $_counter times'),
+        ElevatedButton(
+          onPressed: _incrementCounter,
+          child: Text('Press me'),
+        ),
+      ],
+    );
   }
 }
