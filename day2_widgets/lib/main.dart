d505c820ce0ab1dcec7e68232da9f4ae623e1bb5
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-15 14:23:37 +0530

    stateless widgets

--- day2_widgets/lib/main.dart
+++ day2_widgets/lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
+// A StatelessWidget is a widget that does not require mutable state, meaning that it does not change once it is built. These widgets are immutable, meaning their properties cannot change – all values are final. Stateless widgets are ideal for static content.
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Stateless Widget Example')),
+        body: Center(
+          child: MyStatelessWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello, I am a Stateless Widget');
   }
 }
