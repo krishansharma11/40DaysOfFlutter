2a699ff3f007adb8d1fa0a7dae801f538df54639
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-16 12:21:26 +0530

    margin, padding and alignment are used

--- lib/main.dart
+++ lib/main.dart
@@ -10,71 +10,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: (const Text("Row and Column")),
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: (const Text(
+            "Margin, Padding and Alignment",
+            style: TextStyle(
+                color: Color.fromARGB(255, 127, 13, 5),
+                fontWeight: FontWeight.w600),
+          )),
+        ),
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 50),
+            // margin: EdgeInsets.all(50),
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(50),
+            // padding: EdgeInsets.only(left: 10),
+            height: 200,
+            width: 200,
+            color: Colors.red,
+            child: const Text(
+              "Margin, Padding, Alignment",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
           ),
-          body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Column"),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.green,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 100,
-                  width: 80,
-                  color: Colors.yellow,
-                ),
-                Row(
-                  children: [
-                    Text("Row"),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      height: 100,
-                      width: 100,
-                      color: Colors.red,
-                    ),
-                  ],
-                ),
-                Stack(children: [
-                  Text("Stack"),
-                  Container(
-                    margin: EdgeInsets.only(left: 50),
-                    height: 250,
-                    width: 250,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 50),
-                    height: 200,
-                    width: 200,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 50),
-                    height: 100,
-                    width: 100,
-                    color: Colors.red,
-                  ),
-                ])
-              ])),
+        ),
+      ),
     );
   }
 }
