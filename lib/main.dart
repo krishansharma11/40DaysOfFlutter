148dbaa1486980d6c74cee8e3a115fefac6efabf
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-16 13:40:29 +0530

    image with local and url and icon showing

--- lib/main.dart
+++ lib/main.dart
@@ -8,54 +8,40 @@ class ButtonClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Buttons Examples"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text("Elevated Button"),
-              ElevatedButton(
-                onPressed: () {
-                  print("Elevated Button Pressed");
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue, // Background color
-                  foregroundColor: Colors.white, // Text color
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Images and Icons"),
+            ),
+            body: Container(
+                //1. create new folder with name of assets in lib folder
+                //2. added image/images in same folder that was you created
+                //3. go to subspec.yaml file and uncomment assets line and add your image path
+                //4. the path such as lib/assets/weather.jpg
+                //5. and use same path here
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //showing image from local
+                Text("Showing image from local"),
+                Image.asset("lib/assets/weather.jpg"),
+                SizedBox(width: 30.0),
+                //for showing image using url
+                Text("Showing image using url"),
+                Image.network(
+                    'https://images.pexels.com/photos/1431822/pexels-photo-1431822.jpeg?auto=compress&cs=tinysrgb&w=800'),
+                // height: 200,
+                // width: 300,
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.favorite,
+                    ),
+                    Icon(Icons.card_travel)
+                  ],
                 ),
-                child: Text("Elevated Button"),
-              ),
-              SizedBox(height: 16), // Spacing between buttons
-              Text("Text Button"),
-              TextButton(
-                onPressed: () {
-                  print("Text Button Pressed");
-                },
-                child: Text("Text Button"),
-              ),
-              SizedBox(height: 16),
-              Text("Outlined Button"),
-              OutlinedButton(
-                onPressed: () {
-                  print("Outlined Button Pressed");
-                },
-                child: Text("Outlined Button"),
-              ),
-              SizedBox(height: 16),
-              Text("Icon Button"),
-              IconButton(
-                icon: Icon(Icons.thumb_up),
-                onPressed: () {
-                  print("Icon Button Pressed");
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+              ],
+            ))));
   }
 }
