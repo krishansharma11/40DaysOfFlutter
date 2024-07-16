5ce997e51b3f902dc5fa1120a66d7f1b083228ff
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-16 09:49:18 +0530

    Added container

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,26 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: (const Text("Hello World")),
       ),
-      body: const Center(
-        child: Text("Hello World"),
+      body: Container(
+        alignment: Alignment.center,
+        height: 200,
+        width: 400,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(vertical: 200, horizontal: 100),
+        decoration: BoxDecoration(
+            color: Colors.lightBlue,
+            border: Border.all(color: Colors.green, width: 3),
+            borderRadius: BorderRadius.circular(4)),
+        // transform: Matrix4.rotationZ(0.1),
+        // clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: const Text(
+          'Complex Container',
+          style: TextStyle(
+            color: Colors.white,
+            backgroundColor: Colors.green,
+            fontSize: 16,
+          ),
+        ),
       ),
     ));
   }
