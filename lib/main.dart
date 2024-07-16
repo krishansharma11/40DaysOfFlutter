8074398e611dcc29f6c473d3bd16821d2502c5c2
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-16 11:13:31 +0530

    row and column

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: (const Text("Hello World")),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        height: 200,
-        width: 400,
-        padding: const EdgeInsets.all(10),
-        margin: const EdgeInsets.symmetric(vertical: 200, horizontal: 100),
-        decoration: BoxDecoration(
-            color: Colors.lightBlue,
-            border: Border.all(color: Colors.green, width: 3),
-            borderRadius: BorderRadius.circular(4)),
-        // transform: Matrix4.rotationZ(0.1),
-        // clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: const Text(
-          'Complex Container',
-          style: TextStyle(
-            color: Colors.white,
-            backgroundColor: Colors.green,
-            fontSize: 16,
+      home: Scaffold(
+          appBar: AppBar(
+            title: (const Text("Row and Column")),
           ),
-        ),
-      ),
-    ));
+          body: Container(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 100,
+                  width: 80,
+                  color: Colors.yellow,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                  ],
+                )
+              ]))),
+    );
   }
 }
