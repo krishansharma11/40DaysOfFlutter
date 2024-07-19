f5c419325e7c211c28cfab4187b2735cb78c55d8
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-19 17:40:46 +0530

    floadting action button adding

--- lib/main.dart
+++ lib/main.dart
@@ -19,70 +19,79 @@ class MyListViewClass extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.black45,
-              title: const Text(
-                "List View",
-              ),
+      appBar: AppBar(
+        backgroundColor: Colors.black45,
+        title: const Text(
+          "List View",
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return (ListTile(
+                    title: Text("Item $index"),
+                    subtitle: Text("Sub Item Name"),
+                    leading: Icon(Icons.arrow_forward),
+                    trailing: Icon(Icons.laptop_chromebook),
+                  ));
+                }),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.grey,
+            width: 200,
+            height: 40,
+            alignment: Alignment.center,
+            child: const Text(
+              "Separator List",
+              style: TextStyle(color: Colors.red),
             ),
-            body: Column(
-              children: [
-                SizedBox(
-                  height: 200,
-                  child: ListView.builder(
-                      itemCount: 100,
-                      itemBuilder: (context, index) {
-                        return (ListTile(
-                          title: Text("Item $index"),
-                          subtitle: Text("Sub Item Name"),
-                          leading: Icon(Icons.arrow_forward),
-                          trailing: Icon(Icons.laptop_chromebook),
-                        ));
-                      }),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  color: Colors.grey,
-                  width: 200,
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    "Separator List",
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-                Expanded(
-                  child: ListView.separated(
-                      itemCount: 100,
-                      separatorBuilder: (context, index) => const Divider(),
-                      itemBuilder: (context, index) {
-                        return (ListTile(title: (Text("Item $index"))));
-                      }),
-                ),
-                Container(
-                  color: Colors.grey,
-                  width: 200,
-                  height: 40,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    "Custom List",
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-                Expanded(child: ListView.custom(
-                  childrenDelegate:
-                      SliverChildBuilderDelegate((context, index) {
-                    return (ListTile(
-                      title: (Text("Item $index")),
-                      onTap: () {
-                        print("Item $index tapped");
-                      },
-                    ));
-                  }),
-                )),
-              ],
-            )));
+          ),
+          Expanded(
+            child: ListView.separated(
+                itemCount: 100,
+                separatorBuilder: (context, index) => const Divider(),
+                itemBuilder: (context, index) {
+                  return (ListTile(title: (Text("Item $index"))));
+                }),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 200,
+            height: 40,
+            alignment: Alignment.center,
+            child: const Text(
+              "Custom List",
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+          Expanded(
+            child: ListView.custom(
+              childrenDelegate: SliverChildBuilderDelegate((context, index) {
+                return (ListTile(
+                  title: (Text("Item $index")),
+                  onTap: () {
+                    print("Item $index tapped");
+                  },
+                ));
+              }),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Add item");
+        },
+        child: Icon(Icons.add),
+        tooltip: ("Add More Items"),
+      ),
+    ));
   }
 }
