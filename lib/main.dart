8543883a0c38fe22df8ca89b753cd61c8ca0ab78
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-07-19 17:29:33 +0530

    added custoome list view, separator list view, builder list view

--- lib/main.dart
+++ lib/main.dart
@@ -19,22 +19,70 @@ class MyListViewClass extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text("List View"),
-      ),
-      body: ListView(
-        children: const [
-          ListTile(
-            title: Text("Item 1"),
-          ),
-          ListTile(
-            title: Text("Item 2"),
-          ),
-          ListTile(
-            title: Text("Item 3"),
-          ),
-        ],
-      ),
-    ));
+            appBar: AppBar(
+              backgroundColor: Colors.black45,
+              title: const Text(
+                "List View",
+              ),
+            ),
+            body: Column(
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                      itemCount: 100,
+                      itemBuilder: (context, index) {
+                        return (ListTile(
+                          title: Text("Item $index"),
+                          subtitle: Text("Sub Item Name"),
+                          leading: Icon(Icons.arrow_forward),
+                          trailing: Icon(Icons.laptop_chromebook),
+                        ));
+                      }),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  color: Colors.grey,
+                  width: 200,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Separator List",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.separated(
+                      itemCount: 100,
+                      separatorBuilder: (context, index) => const Divider(),
+                      itemBuilder: (context, index) {
+                        return (ListTile(title: (Text("Item $index"))));
+                      }),
+                ),
+                Container(
+                  color: Colors.grey,
+                  width: 200,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "Custom List",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                Expanded(child: ListView.custom(
+                  childrenDelegate:
+                      SliverChildBuilderDelegate((context, index) {
+                    return (ListTile(
+                      title: (Text("Item $index")),
+                      onTap: () {
+                        print("Item $index tapped");
+                      },
+                    ));
+                  }),
+                )),
+              ],
+            )));
   }
 }
