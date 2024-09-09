a395589c81b291bbeb1d542de440d1c244dce095
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-09-09 17:03:23 +0530

    post api call

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -29,17 +30,29 @@ class BaseApiCalling extends State<ApiCallingScreen> {
     setState(() {
       isLoading = true; // Set loading to true when fetch starts
     });
-    try {
-      final response = await http
-          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
 
-      if (response.statusCode == 200) {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(<String, String>{
+          'title': "post api ",
+          'body': "calling a post api",
+          'userId': "4"
+        }),
+      );
+
+      // Check for a 201 Created status code for POST requests
+      if (response.statusCode == 201) {
         setState(() {
-          dataList = jsonDecode(response.body);
+          dataList = [jsonDecode(response.body)]; // Wrap in a list if needed
+          print("Created: $dataList");
           isLoading = false; // Set loading to false when data is loaded
         });
       } else {
-        throw Exception('Failed to load data');
+        throw Exception(
+            'Failed to load data! Status code: ${response.statusCode}');
       }
     } catch (e) {
       setState(() {
